7d5b944706bc861d6de530769c82f8fe17e3055a
“Ilya <sudek9415@gmail.com>
2021-10-19 14:00:55 +0300

    Added test "TestOneStrike"

--- BowlingGame/Class1.cs
+++ BowlingGame/Class1.cs
@@ -1,18 +1,45 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace BowlingGame
 {
     public class Game
     {
-        private int _score = 0;
-        public void Roll(int i)
+        private int[] _rolls = new int[21];
+        private int _currentRoll = 0;
+        public void Roll(int pins)
         {
-            _score += i;
+            _rolls[_currentRoll++] = pins;
         }
 
         public int Score()
         {
-            return _score;
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (_rolls[frameIndex] == 10)
+                {
+                    score += 10 + _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+                    frameIndex += 1;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 + _rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += _rolls[frameIndex] + _rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+            return score;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
         }
     }
 }
--- BowlingGameTests/UnitTest1.cs
+++ BowlingGameTests/UnitTest1.cs
@@ -6,26 +6,59 @@ namespace BowlingGameTests
 {
     public class Tests
     {
+        private Game _g;
+        
+        [SetUp]
+        protected void SetUp()
+        {
+            _g = new();
+        }
+        
         [Test]
         public void TestGutterGame()
         {
-            var g = new Game();
-            for (int i = 0; i < 20; i++)
-            {
-                g.Roll(0);
-            }
-            Assert.AreEqual(0, g.Score());
+            RollMany(20, 0);
+            Assert.AreEqual(0, _g.Score());
         }
 
         [Test]
         public void TestAllOnes()
         {
-            var g = new Game();
-            for (int i = 0; i < 20; i++)
+            RollMany(20, 1);
+            Assert.AreEqual(20, _g.Score());
+        }
+
+        [Test]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            _g.Roll(3);
+            RollMany(17, 0);
+            Assert.AreEqual(16, _g.Score());
+        }
+
+        [Test]
+        public void TestOneStrike()
+        {
+            _g.Roll(10);
+            _g.Roll(3);
+            _g.Roll(5);
+            RollMany(17, 0);
+            Assert.AreEqual(26, _g.Score());
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
             {
-                g.Roll(1);
+                _g.Roll(pins);
             }
-            Assert.AreEqual(20, g.Score());
+        }
+
+        private void RollSpare()
+        {
+            _g.Roll(3);
+            _g.Roll(7);
         }
     }
 }
